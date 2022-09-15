9dd6e2352029456b7b8cc1f4fc1924bdc3fe1bc0
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-09-15 20:34:15 -0300

    feat(Login): initial instruction

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+import { LoginContainer } from './styles';
+
+export function Login() {
+  return (
+    <LoginContainer>
+      <Text>Hello World</Text>
+    </LoginContainer>
+  );
+}
