e2ae184e73e44d1b45afcad7c45a675aab87fdff
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-10-05 11:10:24 -0300

    feat(highResolutionAssets): config svg and imports absolute

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
+//Avatar
 import man1 from './avatar/man1.png';
 import man2 from './avatar/man2.png';
 import man3 from './avatar/man3.png';
@@ -5,4 +6,19 @@ import woman1 from './avatar/women1.png';
 import woman2 from './avatar/woman2.png';
 import woman3 from './avatar/man3.png';
 
-export { man1, man2, man3, woman1, woman2, woman3 };
+//Accessibility
+//bars
+import BarsWithSupport from './accessibility/bars/barsWithSupport.svg';
+import BarsWithoutSupport from './accessibility/bars/barsWithoutSupport.svg';
+//braille
+
+export {
+  man1,
+  man2,
+  man3,
+  woman1,
+  woman2,
+  woman3,
+  BarsWithSupport,
+  BarsWithoutSupport
+};
