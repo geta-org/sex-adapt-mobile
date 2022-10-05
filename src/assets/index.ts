281acb6e91a8dbf787f29269e7e492098f7558f9
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-10-05 12:12:41 -0300

    feat(highResolutionAssets): adding all accessibility icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -10,7 +10,33 @@ import woman3 from './avatar/man3.png';
 //bars
 import BarsWithSupport from './accessibility/bars/barsWithSupport.svg';
 import BarsWithoutSupport from './accessibility/bars/barsWithoutSupport.svg';
+import BarsOff from './accessibility/bars/barsOff.svg';
 //braille
+import BrailleWithSupport from './accessibility/braille/brailleWithSupport.svg';
+import BrailleWithoutSupport from './accessibility/braille/brailleWithoutSupport.svg';
+import BrailleOff from './accessibility/braille/brailleOff.svg';
+//elevator
+import ElevatorWith from './accessibility/elevator/elevatorWith.svg';
+import ElevatorWithout from './accessibility/elevator/elevatorWithout.svg';
+//stairs
+import StairsWith from './accessibility/stairs/stairsWith.svg';
+//tactileFloor
+import TactileFloorWith from './accessibility/tactileFloor/tactileFloorWith.svg';
+import TactileFloorWithout from './accessibility/tactileFloor/TactileFloorWithout.svg';
+import TactileFloorOff from './accessibility/tactileFloor/tactileFloorOff.svg';
+//pound
+import PoundWithSupport from './accessibility/pound/poundWithSupport.svg';
+import PoundWithoutSupport from './accessibility/pound/poundWithoutSupport.svg';
+import PoundOff from './accessibility/pound/poundOff.svg';
+//floor
+import FloorUneven from './accessibility/floor/floorUneven.svg';
+//door
+import DoorNarrow from './accessibility/door/doorNarrow.svg';
+import DoorWide from './accessibility/door/doorWide.svg';
+import DoorWideOff from './accessibility/door/doorWideOff.svg';
+//ramp
+import RampWith from './accessibility/ramp/rampWith.svg';
+import RampOff from './accessibility/rampOff.svg';
 
 export {
   man1,
@@ -20,5 +46,24 @@ export {
   woman2,
   woman3,
   BarsWithSupport,
-  BarsWithoutSupport
+  BarsWithoutSupport,
+  BarsOff,
+  BrailleWithSupport,
+  BrailleWithoutSupport,
+  BrailleOff,
+  ElevatorWith,
+  ElevatorWithout,
+  StairsWith,
+  TactileFloorWith,
+  TactileFloorWithout,
+  TactileFloorOff,
+  PoundWithSupport,
+  PoundWithoutSupport,
+  PoundOff,
+  FloorUneven,
+  DoorNarrow,
+  DoorWide,
+  DoorWideOff,
+  RampWith,
+  RampOff
 };
