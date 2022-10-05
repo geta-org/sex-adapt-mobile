43bf2cb41759d2af9a9fa437e1350179f6157c05
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-10-05 00:02:15 -0300

    feat(highResolutionAssets): adding the avatars

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+import man1 from './avatar/man1.png';
+import man2 from './avatar/man2.png';
+import man3 from './avatar/man3.png';
+import woman1 from './avatar/women1.png';
+import woman2 from './avatar/woman2.png';
+import woman3 from './avatar/man3.png';
+
+export { man1, man2, man3, woman1, woman2, woman3 };
