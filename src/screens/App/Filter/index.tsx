46090af28923559b03035d98023fc680fd4d7ae9
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-31 11:16:20 -0300

    feat(filter): create switch

--- src/screens/App/Filter/index.tsx
+++ src/screens/App/Filter/index.tsx
@@ -1,3 +1,45 @@
+import { useState } from 'react'
+import { ButtonBase } from 'src/components/Button/ButtonBase'
+import { ButtonRight, FilterContainer, Label, ShowMeWrapper } from './styles'
+
 export function Filter() {
-  return <h1>Hello world!</h1>;
+  const [isEnabled, setIsEnabled] = useState({
+    all: true,
+    favorites: false,
+  })
+
+  function handleToggleAll() {
+    setIsEnabled({
+      all: true,
+      favorites: false,
+    })
+  }
+
+  function handleToggleFavorites() {
+    setIsEnabled({
+      all: false,
+      favorites: true,
+    })
+  }
+
+  return (
+    <FilterContainer>
+      <Label>Me mostre:</Label>
+
+      <ShowMeWrapper>
+        <ButtonBase
+          text="Todos"
+          active={isEnabled.all}
+          onPress={handleToggleAll}
+        />
+        <ButtonRight>
+          <ButtonBase
+            text="Favoritos"
+            active={isEnabled.favorites}
+            onPress={handleToggleFavorites}
+          />
+        </ButtonRight>
+      </ShowMeWrapper>
+    </FilterContainer>
+  )
 }
--- src/screens/App/Filter/styles.ts
+++ src/screens/App/Filter/styles.ts
@@ -1,1 +1,24 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native'
+
+export const FilterContainer = styled.View`
+  margin-top: 16px;
+  padding: 0 32px;
+  flex: 1;
+`
+export const Label = styled.Text`
+  font-family: ${({ theme }) => theme.font_family.bold};
+  font-size: ${({ theme }) => theme.font_size.md};
+  color: ${({ theme }) => theme.colors.gray_1000};
+`
+
+export const ShowMeWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+
+  margin-left: 25px;
+  margin-top: 8px;
+`
+
+export const ButtonRight = styled.View`
+  margin-left: 18px;
+`
