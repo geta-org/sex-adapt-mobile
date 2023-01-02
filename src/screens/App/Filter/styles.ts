cdd34b3bd9c6396f40cd181ecb74769bed0ed42b
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-01-02 05:24:39 -0300

    feat: screen filter

--- src/screens/App/Filter/styles.ts
+++ src/screens/App/Filter/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components/native'
 
 export const FilterContainer = styled.View`
-  margin-top: 16px;
-  padding: 0 32px;
+  padding: 16px 56px 0 32px;
   flex: 1;
+
+  background-color: ${({ theme }) => theme.colors.white};
 `
 export const Label = styled.Text`
   font-family: ${({ theme }) => theme.font_family.bold};
@@ -14,11 +15,19 @@ export const Label = styled.Text`
 export const ShowMeWrapper = styled.View`
   display: flex;
   flex-direction: row;
+  justify-content: space-evenly;
 
-  margin-left: 25px;
-  margin-top: 8px;
+  margin-top: ${({ theme }) => theme.margins.sm};
 `
 
-export const ButtonRight = styled.View`
-  margin-left: 18px;
+export const FilterWrapper = styled.View`
+  justify-content: space-between;
+
+  height: 156px;
+
+  margin-top: 24px;
+`
+export const GoBackWrapper = styled.View`
+  align-self: flex-end;
+  margin-top: 55px;
 `
