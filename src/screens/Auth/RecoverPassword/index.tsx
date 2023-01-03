966eb0dd73a05be00beb7cf5b4a8c953cccffca7
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-01-03 15:19:29 -0300

    feat(recoverPassword): implements inputform

--- src/screens/Auth/RecoverPassword/index.tsx
+++ src/screens/Auth/RecoverPassword/index.tsx
@@ -1,9 +1,7 @@
 import { View } from 'react-native'
-import { TextInput } from 'react-native-paper'
-import { EnvelopeSimple } from 'phosphor-react-native'
 
 import { ButtonBack } from 'src/components/Button/ButtonBack'
-import { InputBase } from 'src/components/Input/InputBase'
+import { InputForm } from 'src/components/Input/InputForm'
 
 import {
   RecoverPasswordWrapper,
@@ -13,7 +11,6 @@ import {
   TextButton,
   FormWrapper,
 } from './styles'
-import { theme } from 'src/styles/theme'
 
 export function RecoverPassword() {
   return (
@@ -25,31 +22,7 @@ export function RecoverPassword() {
       </View>
 
       <FormWrapper>
-        <InputBase
-          label="E-mail"
-          placeholder="Insira seu e-mail"
-          placeholderTextColor={theme.colors.gray_600}
-          selectionColor={theme.colors.gray_600}
-          textColor={theme.colors.gray_1000}
-          underlineColor={theme.colors.gray_600}
-          activeUnderlineColor={theme.colors.gray_600}
-          outlineColor={theme.colors.gray_600}
-          autoCorrect={false}
-          autoCapitalize="none"
-          keyboardType="email-address"
-          returnKeyType="done"
-          left={
-            <TextInput.Icon
-              icon={() => (
-                <EnvelopeSimple
-                  color={theme.colors.red_900}
-                  size={18}
-                  weight="bold"
-                />
-              )}
-            />
-          }
-        />
+        <InputForm icon="mail" label="E-mail" placeholder="john@doe.com" />
       </FormWrapper>
 
       <Button activeOpacity={0.7}>
