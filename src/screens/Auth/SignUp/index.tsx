f8d9e9f882746ac6bf4e90cd7db0f7cbec451237
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-01-04 16:00:43 -0300

    feat(signup): implement dismisskeyboard

--- src/screens/Auth/SignUp/index.tsx
+++ src/screens/Auth/SignUp/index.tsx
@@ -3,6 +3,8 @@ import { useNavigation } from '@react-navigation/native'
 import { ButtonBack } from 'src/components/Button/ButtonBack'
 import { CaretDoubleRight } from 'phosphor-react-native'
 
+import { DismissKeyboard } from 'src/utils/DismissKeyBoard'
+
 import { InputForm } from 'src/components/Input/InputForm'
 
 import {
@@ -24,60 +26,67 @@ export function SignUp() {
   }
 
   return (
-    <SingUpContainer>
-      <ButtonBack />
-      <Header>
-        <Title>Cadastro</Title>
-        <Subtitle>
-          Conte-nos um pouco mais sobre você para realizarmos o seu cadastro
-        </Subtitle>
-      </Header>
+    <DismissKeyboard>
+      <SingUpContainer>
+        <ButtonBack />
+        <Header>
+          <Title>Cadastro</Title>
+          <Subtitle>
+            Conte-nos um pouco mais sobre você para realizarmos o seu cadastro
+          </Subtitle>
+        </Header>
 
-      <FormWrapper>
-        <InputContainer>
-          <Label>Usuário</Label>
-          <InputForm icon="user" label="Nome" placeholder="john doe" />
-        </InputContainer>
+        <FormWrapper>
+          <InputContainer>
+            <Label>Usuário</Label>
+            <InputForm
+              icon="user"
+              label="Nome"
+              placeholder="john doe"
+              returnKeyType="next"
+            />
+          </InputContainer>
 
-        <InputContainer>
-          <Label>E-mail</Label>
-          <InputForm
-            icon="mail"
-            label="Insira seu e-mail"
-            placeholder="john@doe.com"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            returnKeyType="next"
-          />
-        </InputContainer>
+          <InputContainer>
+            <Label>E-mail</Label>
+            <InputForm
+              icon="mail"
+              label="Insira seu e-mail"
+              placeholder="john@doe.com"
+              autoCapitalize="none"
+              keyboardType="email-address"
+              returnKeyType="next"
+            />
+          </InputContainer>
 
-        <InputContainer>
-          <Label>Senha</Label>
-          <InputForm
-            icon="password"
-            label="Senha"
-            placeholder="******"
-            secureTextEntry
-            returnKeyType="next"
-          />
-        </InputContainer>
+          <InputContainer>
+            <Label>Senha</Label>
+            <InputForm
+              icon="password"
+              label="Senha"
+              placeholder="******"
+              secureTextEntry
+              returnKeyType="next"
+            />
+          </InputContainer>
 
-        <InputContainer>
-          <Label>Confirmação da senha</Label>
-          <InputForm
-            icon="password"
-            label="Senha"
-            placeholder="******"
-            secureTextEntry
-            returnKeyType="done"
-          />
-        </InputContainer>
-      </FormWrapper>
+          <InputContainer>
+            <Label>Confirmação da senha</Label>
+            <InputForm
+              icon="password"
+              label="Senha"
+              placeholder="******"
+              secureTextEntry
+              returnKeyType="done"
+            />
+          </InputContainer>
+        </FormWrapper>
 
-      <Button activeOpacity={0.7} onPress={handleAccessibility}>
-        <TextButton>Próximo</TextButton>
-        <CaretDoubleRight size={24} color="#FFF" weight="bold" />
-      </Button>
-    </SingUpContainer>
+        <Button activeOpacity={0.7} onPress={handleAccessibility}>
+          <TextButton>Próximo</TextButton>
+          <CaretDoubleRight size={24} color="#FFF" weight="bold" />
+        </Button>
+      </SingUpContainer>
+    </DismissKeyboard>
   )
 }
